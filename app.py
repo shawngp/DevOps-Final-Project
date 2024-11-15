5379f4a6e48824dd1fa1e0869c8abb234b8409e8
shawngp <82529748+shawngp@users.noreply.github.com>
2024-11-15 22:14:51 +0530

    - logic for translation

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, url_for, request, render_template, session
-
+import requests, os, uuid, json
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -7,3 +9,26 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+    # Read the values from the form
+    original_text = request.form['text']
+    target_language = request.form['language']
+
+    # Load the values from .env
+    key = os.environ['KEY']
+    endpoint = os.environ['ENDPOINT']
+    location = os.environ['LOCATION']
+
+    # Indicate that we want to translate and the API version (3.0) and the target language
+    path = '/translate?api-version=3.0'
+
+    # Add the target language parameter
+    target_language_parameter = '&to=' + target_language
+    
+    # Create the full URL
+    constructed_url = endpoint + path + target_language_parameter
+
+    # Create the body of the request with the text to be translated
+    body = [{ 'text': original_text }]
